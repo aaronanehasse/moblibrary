ff4bdfa9ec0ae2eb2f03fa910a97aaced0806e51
KIOMY <115535759+KI0MY@users.noreply.github.com>
2023-09-17 22:18:03 +0200

    'Trash'

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const app = express()
 const port = 7000
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/application/build')))
+    app.use(express.static(path.join(__dirname, './application/build')))
     
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/application/build', 'index.html'))
+        res.sendFile(path.join(__dirname + './application/build', 'index.html'))
     })
 }
 
